c6a42e57ed62ab6ce11813ee14741422f7ef853d
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-10-25 23:52:46 +0300

    page.tsx

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,33 @@
+import { GetCart } from "@/lib/db/cart";
+import CartEntry from "./CartEntry";
+import { setProductQuantity } from "./action";
+import { formatPrice } from "@/lib/format";
+
+export const metadata = {
+  title: "Your cart - Flowmazon",
+};
+
+export default async function CartPage() {
+  const cart = await GetCart();
+  return (
+    <>
+      <h1 className="mb-6 text-3xl font-bold">Shopping Cart</h1>
+      {cart?.items.map((item) => {
+        return (
+          <CartEntry
+            cartItem={item}
+            key={item.id}
+            setProductQuantity={setProductQuantity}
+          />
+        );
+      })}
+      {!cart?.items.length && <p>Your cart is empty</p>}
+      <div className="flex flex-col items-end sm:items-center">
+        <p className="mb-3 font-bold">
+          Total: {formatPrice(cart?.subTotal || 0)}
+        </p>
+        <button className="btn-primary btn sm:w-[200px]">Checkout</button>
+      </div>
+    </>
+  );
+}
