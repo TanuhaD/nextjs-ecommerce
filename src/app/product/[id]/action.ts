c1ebdcb4facba1aa60e1349677f73a672cd6ae55
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-10-26 23:53:51 +0300

    action.ts

---
+++ src/app/product/[id]/action.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { CreateCart, GetCart } from "@/lib/db/cart";
+import { prisma } from "@/lib/db/prisma";
+import { revalidatePath } from "next/cache";
+
+export async function incrementProductQuantity(productId: string) {
+  const cart = (await GetCart()) ?? (await CreateCart());
+
+  const articleInCart = cart.items.find((item) => item.productId === productId);
+
+  if (articleInCart) {
+    await prisma.cartItem.update({
+      where: { id: articleInCart.id },
+      data: { quantity: { increment: 1 } },
+    });
+  } else {
+    await prisma.cartItem.create({
+      data: {
+        quantity: 1,
+        cartId: cart.id,
+        productId,
+      },
+    });
+  }
+
+  revalidatePath("/product/[id]");
+}
