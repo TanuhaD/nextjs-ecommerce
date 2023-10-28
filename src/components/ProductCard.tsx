3b68f384f20ca04a4e15ed583393b3f6df82f600
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-10-28 23:58:33 +0300

    ProductCard.tsx

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,37 @@
+import { Product } from "@prisma/client";
+import Link from "next/link";
+import Image from "next/image";
+import PriceTag from "./PriceTag";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const isNew =
+    Date.now() - new Date(product.createdAt).getTime() <
+    1000 * 60 * 60 * 24 * 7;
+
+  return (
+    <Link
+      href={`/product/${product.id}`}
+      className="card w-full bg-base-100 transition-shadow hover:shadow-xl"
+    >
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={720}
+        height={100}
+        className="h-48 object-cover"
+      />
+      <div className="card-body">
+        <h2 className="card-title">
+          {product.name}
+          {isNew && <div className="badge badge-secondary">NEW</div>}
+        </h2>
+        <p>{product.description}</p>
+        <PriceTag price={product.price} />
+      </div>
+    </Link>
+  );
+}
