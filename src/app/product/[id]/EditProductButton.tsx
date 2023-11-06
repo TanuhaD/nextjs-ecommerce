6565682ac7ceb58a3b8fab345dbf7a379da416bd
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-06 19:03:03 +0200

    EditProductButton.tsx

---
+++ src/app/product/[id]/EditProductButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+interface EditProductButtonProps {
+  productId: string;
+}
+
+export default function EditProductButton({
+  productId,
+}: EditProductButtonProps): JSX.Element {
+  return (
+    <div className="mt-3 flex items-center gap-2">
+      <Link className="btn-primary btn" href={`/edit-product/${productId}`}>
+        Edit Product
+      </Link>
+    </div>
+  );
+}
