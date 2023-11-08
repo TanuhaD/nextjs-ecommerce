fe2cc8c3e8b7da0b2b83bb0ade2aa1b258c2bc6b
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-08 23:42:40 +0200

    UploadFromFileForm.tsx

--- src/app/add-product/UploadFromFileForm.tsx
+++ src/app/add-product/UploadFromFileForm.tsx
@@ -11,14 +11,23 @@ export default function UploadFromFileForm() {
     let file;
     if (event.target.files) {
       file = event.target.files[0];
-      const reader = new FileReader();
-      reader.readAsText(file, "UTF-8");
-      reader.onload = (evt) => {
-        setJson(JSON.parse(evt.target?.result as string));
-      };
-      reader.onerror = (evt) => {
-        alert("error reading file");
-      };
+      if (file) {
+        const reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = (evt) => {
+          setJson(JSON.parse(evt.target?.result as string));
+        };
+        reader.onerror = (evt) => {
+          MySwal.fire({
+            title: "Error",
+            text: "Error reading file",
+            icon: "error",
+            confirmButtonText: "OK",
+          });
+        };
+      } else {
+        setJson(null);
+      }
     } else {
       MySwal.fire({
         title: "Error",
@@ -39,30 +48,47 @@ export default function UploadFromFileForm() {
       });
       return;
     }
-    const response = await fetch("/api/json-database", {
+    const response = await fetch("/api/create-many-products", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(json),
     });
+
     if (response.ok) {
-      alert("Upload successful");
+      MySwal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Upload successful",
+        showConfirmButton: false,
+        timer: 1500,
+      });
     } else {
       const error = await response.json();
       console.log(error.message);
-      alert("Upload failed");
+      MySwal.fire({
+        title: "Error",
+        text: "Upload failed",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
     }
   };
   return (
-    <div>
+    <div className="m-3 flex justify-center  gap-3">
       <input
+        className="file-input-bordered file-input-warning file-input w-full max-w-xs"
         name="file"
         type="file"
         accept=".json"
+        placeholder="Choose file"
         onChange={handleFileUpload}
       />
-      <button className="btn-primary btn" onClick={handleUploadClickBtn}>
+      <button
+        className="btn-primary btn shadow-md hover:shadow-xl"
+        onClick={handleUploadClickBtn}
+      >
         Upload from file
       </button>
     </div>
