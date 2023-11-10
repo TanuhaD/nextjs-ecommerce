b3d812953601a45532abcfe45d748a213422fc56
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-10 22:13:34 +0200

    DeleteProductButton.tsx

--- src/app/product/[id]/DeleteProductButton.tsx
+++ src/app/product/[id]/DeleteProductButton.tsx
@@ -1,60 +1,66 @@
 "use client";
 
-import { revalidatePath } from "next/cache";
+import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
 interface DeleteProductButtonProps {
   productId: string;
-  userId: string;
 }
 const MySwal = withReactContent(Swal);
 export default function DeleteProductButton({
   productId,
-  userId,
 }: DeleteProductButtonProps): JSX.Element {
+  const router = useRouter();
+
   const handleDeleteProductBtn = async () => {
     const response = await fetch("/api/delete-product-by-id", {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ productId, userId }),
+      body: JSON.stringify({ productId: productId }),
     });
-    if (!response.ok) {
+    if (response.ok) {
+      MySwal.fire({
+        title: "Deleted!",
+        text: "Your product has been deleted.",
+        icon: "success",
+      }).then(() => {
+        window.location.href = "/";
+      });
+    } else {
+      const { message } = await response.json();
       MySwal.fire({
         title: "Error",
-        text: "Error deleting product",
+        text: "Error deleting product: " + message,
         icon: "error",
         confirmButtonText: "OK",
       });
     }
   };
 
+  const hundleClickBtn = () => {
+    MySwal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        handleDeleteProductBtn();
+      }
+    });
+  };
+
   return (
     <div className="mt-3 flex items-center gap-2">
       <button
         className="btn-primary btn shadow-md hover:shadow-xl"
-        onClick={() => {
-          MySwal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!",
-          }).then((result) => {
-            if (result.isConfirmed) {
-              handleDeleteProductBtn();
-              MySwal.fire({
-                title: "Deleted!",
-                text: "Your product has been deleted.",
-                icon: "success",
-              });
-            }
-          });
-        }}
+        onClick={hundleClickBtn}
       >
         Delete Product
       </button>
