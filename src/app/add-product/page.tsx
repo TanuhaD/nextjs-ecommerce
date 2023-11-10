54f8cba3e185fe3f63be81eacae13b76018d96ab
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-10 22:12:02 +0200

    page.tsx

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,69 +1,25 @@
-import FormSubmitButton from "@/components/FormSubmitButton";
-import { prisma } from "@/lib/db/prisma";
 import { checkAdminUser } from "@/lib/checkAdminUser";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { env } from "@/lib/env";
+import { AddProductForm } from "./AddProductForm";
 import UploadFromFileForm from "./UploadFromFileForm";
 export const metadata = {
+  metadataBase: new URL(env.BASE_URL),
   title: "Add Product Flowmazon",
 };
-
-async function addProduct(formData: FormData) {
-  "use server";
-  checkAdminUser();
-  const name = formData.get("name")?.toString();
-  const description = formData.get("description")?.toString();
-  const imageUrl = formData.get("imageUrl")?.toString();
-  const price = Number(formData.get("price") || 0);
-  if (!name || !description || !imageUrl || !price) {
-    throw new Error("Missing property");
-  }
-  await prisma.product.create({
-    data: {
-      name,
-      description,
-      imageUrl,
-      price,
-    },
-  });
-  revalidatePath("/");
-  redirect("/");
-}
 
 export default async function AddProductPage() {
   checkAdminUser();
   return (
     <div>
-      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form action={addProduct}>
-        <input
-          required
-          name="name"
-          placeholder="Name"
-          className="input-bordered input mb-3 w-full shadow-md"
-        />
-        <textarea
-          required
-          name="description"
-          placeholder="Description"
-          className="textarea-bordered textarea mb-3 w-full shadow-md"
-        />
-        <input
-          required
-          name="imageUrl"
-          placeholder="Image Url"
-          type="url"
-          className="input-bordered input mb-3 w-full shadow-md"
-        />
-        <input
-          required
-          name="price"
-          placeholder="Price"
-          type="number"
-          className="input-bordered input mb-3 w-full shadow-md "
-        />
-        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
-      </form>
+      <h1 className="mb-4 text-lg font-bold">Add Product</h1>
+      <h2 className="mb-3 text-center text-lg font-bold">
+        Add product manually
+      </h2>
+      <AddProductForm />
+      <div className="divider"></div>
+      <h2 className="mb-3 text-center text-lg font-bold">
+        Add product from file
+      </h2>
       <UploadFromFileForm />
     </div>
   );
