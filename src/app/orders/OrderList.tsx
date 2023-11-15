54bc470615afbff8343f617c5bb82e3d7715b930
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-15 22:14:07 +0200

    OrderList.tsx

--- src/app/orders/OrderList.tsx
+++ src/app/orders/OrderList.tsx
@@ -8,16 +8,17 @@ interface OrderListProps {
 export default function OrderList({ order }: OrderListProps) {
   return (
     <>
-      <ul className=" p-4">
+      <ul className=" rounded-md p-6 shadow-md ">
         {order.items.map((item) => {
           return <OrderItem key={item.id} item={item} />;
         })}
-        <div className="flex-auto text-center font-bold">
-          Total: {order.total / 100} $
-        </div>
-        <div className="flex-auto text-center font-bold">
-          Created: {order.createdAt.getDate()}.{order.createdAt.getMonth()}.
-          {order.createdAt.getFullYear()}
+        <div className="flex-auto text-end font-bold">
+          <p className="">Total: {order.total / 100} $</p>
+          <p className="">
+            Created: {order.createdAt.getDate()}.{order.createdAt.getMonth()}.
+            {order.createdAt.getFullYear()}
+          </p>
+          <p>Status: {order.status}</p>
         </div>
       </ul>
       <div className="divider"></div>
