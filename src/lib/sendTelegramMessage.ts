31342104239aaf2c17ac27c00195bcb7caa5f803
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-16 20:34:22 +0200

    sendTelegramMessage.ts

--- src/lib/sendTelegramMessage.ts
+++ src/lib/sendTelegramMessage.ts
@@ -1,19 +1,21 @@
 import { env } from "./env";
 
-export async function sendTelegramMessage(message: string) {
+export async function sendTelegramMessage(message: string, parseMode = "HTML") {
   const token = env.TELEGRAM_TOKEN;
   const apiUrl = `https://api.telegram.org/bot${token}/sendMessage`;
-
+  const body = JSON.stringify({
+    chat_id: env.TELEGRAM_CHAT_ID,
+    text: message,
+    parse_mode: parseMode,
+  });
+  console.log(body);
   try {
     const response = await fetch(apiUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        chat_id: env.TELEGRAM_CHAT_ID,
-        text: message,
-      }),
+      body,
     });
 
     const data = await response.json();
