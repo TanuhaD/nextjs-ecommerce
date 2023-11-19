6463389720c5f09aad6f269e24ecba6b44c3acdf
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-19 22:53:58 +0200

    Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer id="footer" className="bg-neutral p-10 text-neutral-content">
       <div className="footer m-auto max-w-7xl">
         <Link href="/add-product">Add Product</Link>
+        <Link href="/orders/admin">All orders</Link>
         <div>
           <span className="footer-title">Services</span>
           <a className="link-hover link">Branding</a>
