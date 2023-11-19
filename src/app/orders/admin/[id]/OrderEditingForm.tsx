9d2aa4cd7bc95d9e5aadfe416aa2b78ea3047011
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-19 22:52:43 +0200

    OrderEditingForm.tsx

--- src/app/orders/admin/[id]/OrderEditingForm.tsx
+++ src/app/orders/admin/[id]/OrderEditingForm.tsx
@@ -17,17 +17,24 @@ export default function OrderEditingForm({
     return <div>Order not found</div>;
   }
 
-  console.log(order);
   return (
-    <div className="mb-6 rounded-md border p-4  shadow-md ">
-      <h2 className=" mb-6 rounded-md border p-2 pt-3  text-center font-bold text-info  sm:text-xl md:text-3xl">
+    <div className=" mb-6  rounded-md border-2 border-solid  border-primary bg-slate-100 p-4 shadow-md ">
+      <h1 className=" mb-6 rounded-md border p-2 pt-3  text-center font-bold text-info  shadow-md sm:text-xl md:text-3xl">
         Order {order.id}
-      </h2>
-      <div className="mb-4 gap-4 p-4">
-        <p className="font-medium">Name: {order.name}</p>
-        <p className="font-medium">Phone: {order.phone} </p>
-        <p className="font-medium">Adress: {order.address}</p>
-        <p className="font-medium">Comments: {order.comments}</p>
+      </h1>
+      <div className="mb-4 gap-6  rounded-md border p-4  shadow-md">
+        <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
+          Name: {order.name}
+        </p>
+        <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
+          Phone: {order.phone}{" "}
+        </p>
+        <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
+          Adress: {order.address}
+        </p>
+        <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
+          Comments: {order.comments}
+        </p>
       </div>
       <ul>
         {order.items.map((item) => {
@@ -36,7 +43,7 @@ export default function OrderEditingForm({
               key={item.id}
               className="mb-3 rounded-md border-2 border-solid border-gray-300 p-3 "
             >
-              <div className="flex gap-4 sm:flex-col  ">
+              <div className="flex items-center gap-4 ">
                 <div className="card-image">
                   <Image
                     src={item.product.imageUrl || "/no-image-placeholder.png"}
@@ -47,24 +54,36 @@ export default function OrderEditingForm({
                   />
                 </div>
                 <div className=" gap-4">
-                  <p className="font-medium text-info">{item.product.name}</p>
-                  <p className="font-medium">Price: {item.price / 100} $</p>
-                  <p className="font-medium">Quantity: {item.quantity}</p>
+                  <p className="font-medium text-info sm:text-sm md:text-2xl">
+                    {item.product.name}
+                  </p>
+                  <p className="font-medium sm:text-sm md:text-2xl">
+                    Price: {item.price / 100} $
+                  </p>
+                  <p className="font-medium  sm:text-sm md:text-2xl">
+                    Quantity: {item.quantity}
+                  </p>
                 </div>
               </div>
             </li>
           );
         })}
       </ul>
-      <div className="flex flex-col items-end justify-center gap-4 p-4 font-semibold">
-        <p>Total: {order.total / 100} $</p>
-        <p className="">
+      <div className="flex flex-col  justify-center gap-4 p-4 font-semibold">
+        <p className="font-bold sm:text-sm md:text-2xl">
+          Total: {order.total / 100} $
+        </p>
+        <p className="font-bold sm:text-sm md:text-2xl">
           Created: {order.createdAt.getDate()}.{order.createdAt.getMonth()}.
           {order.createdAt.getFullYear()}
         </p>
-        <p>Status: {order.status}</p>
+        <p className="font-bold sm:text-sm md:text-2xl">
+          Status: {order.status}
+        </p>
       </div>
-      <button className="btn-primary btn">Change order</button>
+      <div className="flex justify-end">
+        <button className="btn-primary btn ">Change order</button>
+      </div>
     </div>
   );
 }
