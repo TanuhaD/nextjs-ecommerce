0fb433b17a45594e94c476d8b1152a5183ba1d24
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-23 01:06:37 +0200

    OrderEditingForm.tsx

--- src/app/orders/admin/[id]/OrderEditingForm.tsx
+++ src/app/orders/admin/[id]/OrderEditingForm.tsx
@@ -2,7 +2,7 @@
 
 import { GetOrderByIdResult } from "@/lib/db/order";
 import { useEffect, useMemo, useState } from "react";
-import OrderInfoProduct from "./change-order/OrderInfoProduct";
+import OrderInfoProduct from "./OrderInfoProduct";
 
 interface OrderEditingFormProps {
   orderResult: GetOrderByIdResult;
@@ -14,28 +14,54 @@ export default function OrderEditingForm({
   const [orderItems, setOrderItems] = useState(order?.items || []);
   const [name, setName] = useState(order?.name || "");
   const [phone, setPhone] = useState(order?.phone || "");
-  const [adress, setAdress] = useState(order?.address || "");
+  const [address, setAddress] = useState(order?.address || "");
   const [comments, setComments] = useState(order?.comments || "");
+  const [total, setTotal] = useState(order?.total || 0);
+  const [status, setStatus] = useState(order?.status || "");
 
   useEffect(() => {
     console.log(orderItems);
+    setTotal(
+      orderItems.reduce((acc, item) => {
+        return acc + item.price * item.quantity;
+      }, 0)
+    );
   }, [orderItems]);
 
-  const handleItemsQuantityChange = useMemo(
-    () =>
-      ({ itemId, newQuantity }: { itemId: string; newQuantity: string }) => {
-        console.log("newQuantity", newQuantity);
-        setOrderItems((prev) => {
-          const newOrders = prev.map((order) =>
-            order.id === itemId
-              ? { ...order, quantity: parseInt(newQuantity) || 0 }
-              : order
-          );
-          return newOrders;
-        });
-      },
-    []
-  );
+  const handleItemsQuantityChange = ({
+    itemId,
+    newQuantity,
+  }: {
+    itemId: string;
+    newQuantity: string;
+  }) => {
+    console.log("newQuantity", newQuantity);
+    setOrderItems((prev) => {
+      const newOrders = prev.map((order) =>
+        order.id === itemId
+          ? { ...order, quantity: parseInt(newQuantity) || 0 }
+          : order
+      );
+      return newOrders;
+    });
+  };
+
+  const handleOrderChange = () => {
+    const newOrder = {
+      ...order,
+      items: orderItems,
+      name,
+      phone,
+      address: address,
+      comments,
+      total,
+    };
+    console.log("newOrder", newOrder);
+    fetch("/api/update-order-admin", {
+      method: "POST",
+      body: JSON.stringify(newOrder),
+    });
+  };
 
   if (error) {
     return <div>{error}</div>;
@@ -46,15 +72,15 @@ export default function OrderEditingForm({
 
   return (
     <div className=" mb-6  rounded-md border-2 border-solid  border-primary bg-slate-50 p-4 shadow-md ">
-      <h1 className=" mb-6 rounded-md border p-2 pt-3  text-center font-bold text-info  shadow-md sm:text-xl md:text-3xl">
+      <h1 className=" mb-6 rounded-md border p-2 pt-3  text-center font-bold text-info  shadow-md sm:text-xs md:text-3xl">
         Order {order.id}
       </h1>
       <div className="mb-4 gap-6  rounded-md border p-4  shadow-md">
-        <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
+        <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl lg:text-lg">
           Name:{" "}
           <input
             type="text"
-            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            className="input-ghost input ml-2 w-full max-w-xs sm:text-sm md:text-2xl"
             onChange={(e) => {
               setName(e.target.value);
             }}
@@ -65,7 +91,7 @@ export default function OrderEditingForm({
           Phone:
           <input
             type="text"
-            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            className="input-ghost input ml-2 w-full max-w-xs sm:text-sm md:text-2xl"
             onChange={(e) => {
               setPhone(e.target.value);
             }}
@@ -73,21 +99,21 @@ export default function OrderEditingForm({
           />
         </p>
         <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
-          Adress:
+          Address:
           <input
             type="text"
-            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            className="input-ghost input ml-2 w-full max-w-xs sm:text-sm md:text-2xl"
             onChange={(e) => {
-              setAdress(e.target.value);
+              setAddress(e.target.value);
             }}
-            value={adress}
+            value={address}
           />
         </p>
         <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
-          Comments: {order.comments}
+          Comments:
           <input
             type="text"
-            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            className="input-ghost input ml-2 w-full max-w-xs sm:text-sm md:text-2xl"
             onChange={(e) => {
               setComments(e.target.value);
             }}
@@ -108,18 +134,34 @@ export default function OrderEditingForm({
       </ul>
       <div className="flex flex-col  justify-center gap-4 p-4 font-semibold">
         <p className="font-bold sm:text-sm md:text-2xl">
-          Total: {order.total / 100} $
+          Total: {total / 100} $
         </p>
         <p className="font-bold sm:text-sm md:text-2xl">
           Created: {order.createdAt.getDate()}.{order.createdAt.getMonth()}.
           {order.createdAt.getFullYear()}
         </p>
         <p className="font-bold sm:text-sm md:text-2xl">
-          Status: {order.status}
+          Status: {status}
+          <select
+            className="select-warning select mb-3 ml-3 mt-3 w-full max-w-xs sm:text-sm md:text-2xl"
+            defaultValue={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            <option>Pending</option>
+            <option>Processing</option>
+            <option>Dispatched</option>
+            <option>Delivered</option>
+            <option>Canceled</option>
+            <option>Refunded</option>
+            <option>Returned</option>
+            <option>Completed</option>
+          </select>
         </p>
       </div>
       <div className="flex justify-end">
-        <button className="btn-primary btn ">Change order</button>
+        <button onClick={handleOrderChange} className="btn-primary btn ">
+          Change order
+        </button>
       </div>
     </div>
   );
