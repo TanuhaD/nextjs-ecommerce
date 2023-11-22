53f726b08cfa897310fd733c6080d60f4bbc5282
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-22 12:42:34 +0200

    OrderAdminSearch.tsx

--- src/app/orders/admin/OrderAdminSearch.tsx
+++ src/app/orders/admin/OrderAdminSearch.tsx
@@ -1,7 +1,18 @@
+import { redirect } from "next/navigation";
+
+async function searchOrders(formData: FormData) {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+  if (searchQuery) {
+    redirect("/orders/admin/search?query=" + searchQuery);
+  }
+}
+
 export default function OrderAdminSearch() {
   return (
     <>
-      <form className="mb-6">
+      <form action={searchOrders} className="mb-6">
         <div className="form-control">
           <input
             className=" input-bordered input m-auto min-w-[100px] max-w-[300px]"
