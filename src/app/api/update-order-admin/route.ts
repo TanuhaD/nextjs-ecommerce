3959968d9bc239bcce97322014a5d969b7dba2a9
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-23 01:06:13 +0200

    route.ts

--- src/app/api/update-order-admin/route.ts
+++ src/app/api/update-order-admin/route.ts
@@ -1,30 +1,36 @@
-import { OrderWithProducts } from "@/lib/db/order";
+import { OrderItemWithProduct, OrderWithProducts } from "@/lib/db/order";
 import { prisma } from "@/lib/db/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const { id } = await request.json();
   try {
+    const body = await request.json();
     const order = await prisma.order.update({
-      where: { id: id },
+      where: { id: body.id },
       data: {
-        name: "name",
-        phone: "phone",
-        adress: "adress",
-        comments: "comments",
-        status: "status",
-        total: 123,
-      },
-      items: {
-        update: {},
+        name: body.name,
+        phone: body.phone,
+        address: body.address,
+        comments: body.comments,
+        status: body.status,
+        total: body.total,
+        items: {
+          updateMany: body.items.map((item: OrderItemWithProduct) => {
+            return {
+              where: { id: item.id },
+              data: {
+                quantity: item.quantity,
+                price: item.price,
+              },
+            };
+          }),
+        },
       },
     });
 
-    return NextResponse.json(
-      { message: "There should be an array of objects in the request body" },
-      { status: 400 }
-    );
+    return NextResponse.json({ order }, { status: 200 });
   } catch (error: any) {
+    console.log(error);
     return NextResponse.json({ message: error.message }, { status: 400 });
   }
 }
