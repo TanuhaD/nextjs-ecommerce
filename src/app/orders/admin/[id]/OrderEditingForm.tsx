cdf98eb57fcfa9568c87813d3652e31bdfefb29f
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-22 12:42:55 +0200

    OrderEditingForm.tsx

--- src/app/orders/admin/[id]/OrderEditingForm.tsx
+++ src/app/orders/admin/[id]/OrderEditingForm.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { GetOrderByIdResult } from "@/lib/db/order";
-import Image from "next/image";
+import { useEffect, useMemo, useState } from "react";
+import OrderInfoProduct from "./change-order/OrderInfoProduct";
 
 interface OrderEditingFormProps {
   orderResult: GetOrderByIdResult;
@@ -10,6 +11,32 @@ interface OrderEditingFormProps {
 export default function OrderEditingForm({
   orderResult: { order, error },
 }: OrderEditingFormProps) {
+  const [orderItems, setOrderItems] = useState(order?.items || []);
+  const [name, setName] = useState(order?.name || "");
+  const [phone, setPhone] = useState(order?.phone || "");
+  const [adress, setAdress] = useState(order?.address || "");
+  const [comments, setComments] = useState(order?.comments || "");
+
+  useEffect(() => {
+    console.log(orderItems);
+  }, [orderItems]);
+
+  const handleItemsQuantityChange = useMemo(
+    () =>
+      ({ itemId, newQuantity }: { itemId: string; newQuantity: string }) => {
+        console.log("newQuantity", newQuantity);
+        setOrderItems((prev) => {
+          const newOrders = prev.map((order) =>
+            order.id === itemId
+              ? { ...order, quantity: parseInt(newQuantity) || 0 }
+              : order
+          );
+          return newOrders;
+        });
+      },
+    []
+  );
+
   if (error) {
     return <div>{error}</div>;
   }
@@ -18,54 +45,64 @@ export default function OrderEditingForm({
   }
 
   return (
-    <div className=" mb-6  rounded-md border-2 border-solid  border-primary bg-slate-100 p-4 shadow-md ">
+    <div className=" mb-6  rounded-md border-2 border-solid  border-primary bg-slate-50 p-4 shadow-md ">
       <h1 className=" mb-6 rounded-md border p-2 pt-3  text-center font-bold text-info  shadow-md sm:text-xl md:text-3xl">
         Order {order.id}
       </h1>
       <div className="mb-4 gap-6  rounded-md border p-4  shadow-md">
         <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
-          Name: {order.name}
+          Name:{" "}
+          <input
+            type="text"
+            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+          />
         </p>
         <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
-          Phone: {order.phone}{" "}
+          Phone:
+          <input
+            type="text"
+            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            value={phone}
+          />
         </p>
         <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
-          Adress: {order.address}
+          Adress:
+          <input
+            type="text"
+            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            onChange={(e) => {
+              setAdress(e.target.value);
+            }}
+            value={adress}
+          />
         </p>
         <p className=" border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl ">
           Comments: {order.comments}
+          <input
+            type="text"
+            className="input-ghost input w-full max-w-xs sm:text-sm md:text-2xl"
+            onChange={(e) => {
+              setComments(e.target.value);
+            }}
+            value={comments}
+          />
         </p>
       </div>
       <ul>
         {order.items.map((item) => {
           return (
-            <li
+            <OrderInfoProduct
               key={item.id}
-              className="mb-3 rounded-md border-2 border-solid border-gray-300 p-3 "
-            >
-              <div className="flex items-center gap-4 ">
-                <div className="card-image">
-                  <Image
-                    src={item.product.imageUrl || "/no-image-placeholder.png"}
-                    alt={item.product.name}
-                    width={100}
-                    height={100}
-                    className="rounded-lg"
-                  />
-                </div>
-                <div className=" gap-4">
-                  <p className="font-medium text-info sm:text-sm md:text-2xl">
-                    {item.product.name}
-                  </p>
-                  <p className="font-medium sm:text-sm md:text-2xl">
-                    Price: {item.price / 100} $
-                  </p>
-                  <p className="font-medium  sm:text-sm md:text-2xl">
-                    Quantity: {item.quantity}
-                  </p>
-                </div>
-              </div>
-            </li>
+              item={item}
+              quantityChangeHandler={handleItemsQuantityChange}
+            />
           );
         })}
       </ul>
