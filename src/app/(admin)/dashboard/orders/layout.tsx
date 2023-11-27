828336065cad5d5257b46fdedd511babaa83c0a6
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-27 23:32:19 +0200

    layout.tsx

---
+++ src/app/(admin)/dashboard/orders/layout.tsx
@@ -0,0 +1,17 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import AdminOrderContextProvider from "./AdminOrderContext";
+
+export default async function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+  if (session?.user.role !== "ADMIN") {
+    redirect("/forbidden");
+  }
+  return <AdminOrderContextProvider>{children}</AdminOrderContextProvider>;
+}
