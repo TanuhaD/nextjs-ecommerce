2cf349774026780c2c2d6e0d7befd92be33b7132
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-27 23:32:46 +0200

    AdminOrderContext.tsx

---
+++ src/app/(admin)/dashboard/orders/AdminOrderContext.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { createContext } from "react";
+export const AdminOrderContext = createContext<string | null>(null);
+
+interface AdminOrderContextProviderProps {
+  children: React.ReactNode;
+}
+
+export default function AdminOrderContextProvider({
+  children,
+}: AdminOrderContextProviderProps) {
+  return (
+    <AdminOrderContext.Provider value={null}>
+      {children}
+    </AdminOrderContext.Provider>
+  );
+}
