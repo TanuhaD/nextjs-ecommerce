3a3e6fbaf3dd6f82d77a8e2d5ed72ef2db6a17ec
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-30 00:33:55 +0200

    edit-update-server-action-response.ts

--- src/types/edit-update-server-action-response.ts
+++ src/types/edit-update-server-action-response.ts
@@ -3,7 +3,10 @@ import { Prisma } from "@prisma/client";
 export interface EditUpdateServerActionResponse {
   result: "CREATED" | "UPDATED" | "FAIL" | null;
   error: string | null;
-  prismaResult: Prisma.ProductCreateInput | Prisma.ProductUpdateInput | null;
+  prismaResult:
+    | Prisma.ProductCreateInput
+    | (Prisma.ProductUpdateInput & { id: string })
+    | null;
 }
 
 export interface EditUpdateServerActionResponseOrder {
