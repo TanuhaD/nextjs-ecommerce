6bf755d9841162caeb285ed19cb628df70e3abd3
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-30 00:32:06 +0200

    PaginationBar.tsx

--- src/components/PaginationBar/PaginationBar.tsx
+++ src/components/PaginationBar/PaginationBar.tsx
@@ -19,30 +19,32 @@ export default function PaginationBar({
       <Link
         key={page}
         href={`?page=${page}`}
-        className={`join-item btn ${
+        className={`btn join-item ${
           page === currentPage ? "btn-active pointer-events-none" : ""
         }`}
         prefetch={false}
       >
         {page}
-      </Link>
+      </Link>,
     );
   }
   return (
     <>
-      <div className="join hidden sm:block">{numberedPageItems}</div>
+      <div className="my-pagination-style join hidden  bg-gray-400  hover:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700 sm:block">
+        {numberedPageItems}
+      </div>
       <div className=" join block sm:hidden">
         {currentPage > 1 && (
-          <Link href={"?page=" + (currentPage - 1)} className="join-item btn">
+          <Link href={"?page=" + (currentPage - 1)} className="btn join-item  ">
             Prev
           </Link>
         )}
 
-        <button className="join-item btn pointer-events-none">
-          Page{currentPage}
+        <button className="btn join-item pointer-events-none ">
+          {currentPage}
         </button>
         {currentPage < totalPages && (
-          <Link href={"?page=" + (currentPage + 1)} className="join-item btn">
+          <Link href={"?page=" + (currentPage + 1)} className="btn join-item ">
             Next
           </Link>
         )}
