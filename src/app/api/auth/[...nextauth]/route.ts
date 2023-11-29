bcc38086fb02b0925b33dfc42211106d47e3f656
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-30 00:30:09 +0200

    route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,45 +1,5 @@
-import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
-import { prisma } from "@/lib/db/prisma";
-import { env } from "@/lib/env";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
-import GoogleProvaider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
-  providers: [
-    GoogleProvaider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-    GitHubProvider({
-      clientId: env.GITHUB_ID,
-      clientSecret: env.GITHUB_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id;
-      session.user.role = user.role;
-      return session;
-    },
-  },
-  events: {
-    signIn({ user }) {
-      mergeAnonymousCartIntoUserCart(user.id);
-    },
-  },
-};
+import { authOptions } from "../authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
