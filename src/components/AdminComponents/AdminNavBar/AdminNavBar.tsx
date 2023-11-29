ccc9c2b96624df421f7305c815902dc492d28e97
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-11-30 00:30:57 +0200

    AdminNavBar.tsx

--- src/components/AdminComponents/AdminNavBar/AdminNavBar.tsx
+++ src/components/AdminComponents/AdminNavBar/AdminNavBar.tsx
@@ -4,31 +4,27 @@ import MenuIcon from "@/components/MenuIcon";
 import { closeDropdowns } from "@/lib/closeDropdowns";
 import Link from "next/link";
 import { useRef, useState } from "react";
+import AdminNavBarMobMenu from "./AdminNavBarMobMenu";
 
 export default function AdminNavBar() {
   return (
     <div className="navbar  flex justify-between bg-base-100">
       <h1 className="btn btn-ghost text-xl">Admin panel</h1>
 
-      <div className="dropdown  dropdown-end flex">
-        <button className=" mx-2 block cursor-pointer md:hidden">
-          <MenuIcon />
-        </button>
-        <ul
-          tabIndex={0}
-          className="menu dropdown-content z-[1] mt-[320px] w-52 rounded-box bg-base-100 p-2 shadow"
-        >
-          <li className="mx-4 my-6 md:my-0" onClick={closeDropdowns}>
-            <Link href={"/dashboard/orders"}>All orders</Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0" onClick={closeDropdowns}>
-            <Link href={"/dashboard/add-product"}>Add Product</Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0" onClick={closeDropdowns}>
-            <Link href={"/dashboard"}>All Products</Link>
-          </li>
-        </ul>
+      <div className="md:hidden">
+        <AdminNavBarMobMenu />
       </div>
+      <ul className="hidden font-bold md:flex">
+        <li className="mx-4 my-6 md:my-0" onClick={closeDropdowns}>
+          <Link href={"/dashboard/orders"}>All orders</Link>
+        </li>
+        <li className="mx-4 my-6 md:my-0" onClick={closeDropdowns}>
+          <Link href={"/dashboard/add-product"}>Add Product</Link>
+        </li>
+        <li className="mx-4 my-6 md:my-0" onClick={closeDropdowns}>
+          <Link href={"/dashboard"}>All Products</Link>
+        </li>
+      </ul>
     </div>
   );
 }
