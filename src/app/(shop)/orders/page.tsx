0c88eb27ace115305300a741e0799e6d881b9327
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-12-01 22:57:25 +0200

    page.tsx

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -5,18 +5,38 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import OrderList from "./OrderList";
 import { authOptions } from "@/app/api/auth/authOptions";
+import PaginationBar from "@/components/PaginationBar/PaginationBar";
+import { prisma } from "@/lib/db/prisma";
 
 export const metadata = {
   metadataBase: new URL(env.BASE_URL),
   title: "Your order - Flowmazon",
 };
 
-export default async function OrderPage() {
+interface OrderProps {
+  searchParams: { page: string };
+}
+export default async function OrderPage({
+  searchParams: { page = "1" },
+}: OrderProps) {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/add-product");
   }
   const { orders, error } = await GetOrdersByUserId(session.user.id);
+  const currentPage = parseInt(page);
+
+  const pageSize = 6;
+  const heroItemCount = 1;
+
+  const totalItemCount = orders?.length || 0;
+
+  const totalPages = Math.ceil((totalItemCount - heroItemCount) / pageSize);
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  const paginatedOrders = orders?.slice(startIndex, endIndex);
 
   return (
     <>
@@ -24,9 +44,15 @@ export default async function OrderPage() {
         Your orders {session.user.name}
       </h2>
 
-      {orders?.map((order) => {
+      {paginatedOrders?.map((order) => {
         return <OrderList key={order.id} order={order} />;
       })}
+
+      <div className="m-auto flex justify-center">
+        {totalPages > 1 && (
+          <PaginationBar currentPage={currentPage} totalPages={totalPages} />
+        )}
+      </div>
     </>
   );
 }
