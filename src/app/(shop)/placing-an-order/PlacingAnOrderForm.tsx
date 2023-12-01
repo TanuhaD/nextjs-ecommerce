4566f5ecd407078d578de8f225677cada3c6d833
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-12-01 22:57:54 +0200

    PlacingAnOrderForm.tsx

--- src/app/(shop)/placing-an-order/PlacingAnOrderForm.tsx
+++ src/app/(shop)/placing-an-order/PlacingAnOrderForm.tsx
@@ -5,44 +5,34 @@ import { MySwal } from "@/lib/sweet-alert";
 import { useFormState, useFormStatus } from "react-dom";
 import { useRouter } from "next/navigation";
 import { ZodError, z } from "zod";
+import { useFormValidationHook } from "./useFormValidationHook";
+import { PlaceOrderResult } from "./action";
+import { stat } from "fs";
 
-export interface validationFields {
-  name?: string;
-  phone?: string;
-  email?: string;
-  address?: string;
-  comments?: string;
-}
-
-const initialState = {
+const initialState: PlaceOrderResult = {
+  validationErrors: null,
   result: null,
   error: null,
-  prismaResult: null,
 };
 
-const schema = z.object({
-  name: z.string().min(3),
-  phone: z.string().min(3),
-  email: z.string().email(),
-  address: z.string().min(3),
-  comments: z.string().max(1000),
-});
-
 export const PlacingAnOrderForm: React.FC<{
-  action: (
-    _: any,
-    formData: FormData,
-  ) => Promise<{
-    result: string | null;
-    error: string | null;
-  }>;
+  action: (_: any, formData: FormData) => Promise<PlaceOrderResult>;
 }> = ({ action }) => {
   const [state, formAction] = useFormState(action, initialState);
-  const [nameError, setNameError] = useState("");
+
   const { pending } = useFormStatus();
   const router = useRouter();
+  const {
+    nameValidationMessage,
+    emailValidationMessage,
+    phoneValidationMessage,
+    addressValidationMessage,
+    commentsValidationMessage,
+    removeValidationMessage,
+  } = useFormValidationHook(state);
+
   useEffect(() => {
-    const result = state.result || "";
+    const result = state?.result || "";
 
     if (result === "CREATED") {
       MySwal.fire({
@@ -52,7 +42,7 @@ export const PlacingAnOrderForm: React.FC<{
       }).then(() => {
         router.push(`/orders`);
       });
-    } else if (result === "FAIL") {
+    } else if (result === "FAIL" && !state?.validationErrors) {
       MySwal.fire({
         icon: "error",
         title: "Error. Please try again.",
@@ -62,73 +52,92 @@ export const PlacingAnOrderForm: React.FC<{
     }
   }, [state, router]);
 
-  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    const formData = new FormData(event.currentTarget);
-    const formValues = Object.fromEntries(formData.entries());
-
-    try {
-      schema.parse(formValues);
-    } catch (e) {
-      event.preventDefault();
-      const error = e as z.ZodError;
-      const validationErrors: validationFields = {};
-      error.errors.forEach((err) => {
-        const field = err.path.join(".") as keyof validationFields;
-        validationErrors[field] = err.message;
-      });
-      if (validationErrors.name) {
-        setNameError(validationErrors.name);
-      }
-    }
-  };
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNameError("");
-  };
   return (
     <div className=" flex justify-center">
-      <form
-        onSubmit={handleFormSubmit}
-        className="max-w-md"
-        action={formAction}
-      >
+      <form className="max-w-md" action={formAction}>
         <label className="form-control mb-3">
           <input
-            onChange={handleNameChange}
+            onChange={() => removeValidationMessage("name")}
             required
             name="name"
+            autoComplete="name"
             placeholder="Name"
             type="text"
             className="input input-bordered  w-full shadow-md"
           />
           <div className="label">
-            <span className="label-text-alt text-red-700">{nameError}</span>
+            {nameValidationMessage && (
+              <span className="label-text-alt text-red-700">
+                {nameValidationMessage}
+              </span>
+            )}
           </div>
         </label>
 
-        <input
-          required
-          name="phone"
-          type="text"
-          placeholder="Phone"
-          className="input input-bordered mb-3 w-full shadow-md"
-        />
-        <input
-          name="email"
-          placeholder="Email"
-          type="text"
-          className="input input-bordered mb-3 w-full shadow-md"
-        />
-        <input
-          name="address"
-          placeholder="Address"
-          className="input input-bordered mb-3 w-full shadow-md"
-          type="text"
-        />
-        <textarea
-          name="comments"
-          placeholder="Comments on the order"
-          className="textarea textarea-bordered mb-3 w-full shadow-md"
-        />
+        <label className="form-control mb-3">
+          <input
+            onChange={() => removeValidationMessage("phone")}
+            required
+            name="phone"
+            autoComplete="phone"
+            type="text"
+            placeholder="Phone"
+            className="input input-bordered  w-full shadow-md"
+          />
+          <div className="label">
+            {phoneValidationMessage && (
+              <span className="label-text-alt text-red-700">
+                {phoneValidationMessage}
+              </span>
+            )}
+          </div>
+        </label>
+        <label className="form-control mb-3">
+          <input
+            onChange={() => removeValidationMessage("email")}
+            name="email"
+            placeholder="Email"
+            type="text"
+            className="input input-bordered  w-full shadow-md"
+          />
+          <div className="label">
+            {emailValidationMessage && (
+              <span className="label-text-alt text-red-700">
+                {emailValidationMessage}
+              </span>
+            )}
+          </div>
+        </label>
+        <label className="form-control mb-3">
+          <input
+            onChange={() => removeValidationMessage("address")}
+            name="address"
+            placeholder="Address"
+            className="input input-bordered  w-full shadow-md"
+            type="text"
+          />
+          <div className="label">
+            {addressValidationMessage && (
+              <span className="label-text-alt text-red-700">
+                {addressValidationMessage}
+              </span>
+            )}
+          </div>
+        </label>
+        <label className="form-control mb-3">
+          <textarea
+            name="comments"
+            placeholder="Comments on the order"
+            className="textarea textarea-bordered mb-3 w-full shadow-md"
+          />
+          <div className="label">
+            {commentsValidationMessage && (
+              <span className="label-text-alt text-red-700">
+                {commentsValidationMessage}
+              </span>
+            )}
+          </div>
+        </label>
 
         <button
           className={`btn btn-primary btn-block shadow-md hover:shadow-xl`}
