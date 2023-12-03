506ec3f31d9e4d3a38b93118d6efb5e10801b51c
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-12-04 00:12:49 +0200

    OrderAdminSearch.tsx

--- src/app/(admin)/dashboard/orders/OrderAdminSearch.tsx
+++ src/app/(admin)/dashboard/orders/OrderAdminSearch.tsx
@@ -15,7 +15,7 @@ export default function OrderAdminSearch() {
       <form action={searchOrders} className="mb-6">
         <div className="form-control">
           <input
-            className=" input-bordered input m-auto min-w-[100px] max-w-[300px]"
+            className=" input input-bordered m-auto min-w-[100px] max-w-[300px]"
             name="searchQuery"
             placeholder="Search"
           />
