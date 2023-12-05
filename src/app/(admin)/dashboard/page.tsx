4b83973522361d32767a1aacfe9f89730d6879fe
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-12-05 02:02:15 +0200

    page.tsx

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import AdminProductCard from "@/components/AdminComponents/AdminProductCard";
 import PaginationBar from "@/components/PaginationBar/PaginationBar";
 
 import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
 
 interface DashboardProps {
   searchParams: { page: string };
@@ -26,8 +27,26 @@ const DashboardPage = async ({
     take: pageSize + (currentPage === 1 ? heroItemCount : 0),
   });
 
+  async function searchProducts(formData: FormData) {
+    "use server";
+
+    const searchQuery = formData.get("searchQuery")?.toString();
+    if (searchQuery) {
+      redirect("/dashboard/search?query=" + searchQuery);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center">
+      <form action={searchProducts}>
+        <div className="form-control">
+          <input
+            className=" input input-bordered w-full min-w-[100px]"
+            name="searchQuery"
+            placeholder="Search"
+          />
+        </div>
+      </form>
       <div className="xl:grid-cols-3 my-4 grid grid-cols-1 gap-4 md:grid-cols-2">
         {products.map((product) => {
           return <AdminProductCard key={product.id} product={product} />;
