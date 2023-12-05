44ab31ec3fab81d35538a4f8c1c1dafa26ebfaac
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-12-05 02:03:09 +0200

    page.tsx

---
+++ src/app/(admin)/dashboard/search/page.tsx
@@ -0,0 +1,51 @@
+import AdminProductCard from "@/components/AdminComponents/AdminProductCard";
+import ProductCard from "@/components/ProductCard";
+import { prisma } from "@/lib/db/prisma";
+import { env } from "@/lib/env";
+
+interface SearchPageProps {
+  searchParams: { query: string };
+}
+
+export function generateMetadata({ searchParams: { query } }: SearchPageProps) {
+  return {
+    metadataBase: new URL(env.BASE_URL),
+    title: `Search:${query} - Flowmazon`,
+  };
+}
+
+export default async function SearchPage({
+  searchParams: { query },
+}: SearchPageProps) {
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          },
+        },
+        {
+          description: {
+            contains: query,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+    orderBy: { id: "desc" },
+  });
+
+  if (products.length === 0) {
+    return <div className="text-center"> No products found</div>;
+  }
+
+  return (
+    <div className="xl:grid-cols-3 my-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+      {products.map((product) => {
+        return <AdminProductCard key={product.id} product={product} />;
+      })}
+    </div>
+  );
+}
