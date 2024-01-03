4e9c0fa73f18eecd03288b9d27c8b8bbfa18cc91
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2024-01-03 19:38:13 +0200

    OrderAdminInfoUser.tsx

--- src/app/(admin)/dashboard/orders/OrderAdminInfoUser.tsx
+++ src/app/(admin)/dashboard/orders/OrderAdminInfoUser.tsx
@@ -4,20 +4,12 @@ interface OrderAdminCustomerInfoProps {
   order: OrderWithProductsAndUser;
 }
 
-export default function OrderAdminInfoUser({
-  order,
-}: OrderAdminCustomerInfoProps) {
+export default function OrderAdminInfoUser({ order }: OrderAdminCustomerInfoProps) {
   return (
     <div className="bg-gray-150 mb-4 gap-6  rounded-md border p-4  shadow-md ">
-      <h2 className=" text-bold medium mb-4 text-center sm:text-xl md:text-3xl">
-        User Information
-      </h2>
-      <p className=" break-words border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl">
-        Name: {order.user?.name}
-      </p>
-      <p className="break-words border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-2xl">
-        Email: {order.user?.email}
-      </p>
+      <h2 className=" text-bold medium mb-4 text-center sm:text-xl md:text-2xl">User Information</h2>
+      <p className=" break-words border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-xl">Name: {order.user?.name}</p>
+      <p className="break-words border-b-2 border-gray-200 p-4 font-medium text-info sm:text-sm md:text-xl">Email: {order.user?.email}</p>
     </div>
   );
 }
