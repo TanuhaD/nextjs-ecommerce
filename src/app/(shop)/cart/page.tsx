2d98bd42cc8b383a258ecedcc7c1013ccdc38d1f
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2024-01-26 23:28:25 +0200

    page.tsx

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export const metadata = {
   metadataBase: new URL(env.BASE_URL),
-  title: "Your cart - Flowmazon",
+  title: "Your cart - PrimePicks",
 };
 
 export default async function CartPage() {
@@ -17,33 +17,19 @@ export default async function CartPage() {
     <>
       <h1 className="mb-6 text-3xl font-bold">Shopping Cart</h1>
       {cart?.items.map((item) => {
-        return (
-          <CartEntry
-            cartItem={item}
-            key={item.id}
-            setProductQuantity={setProductQuantity}
-          />
-        );
+        return <CartEntry cartItem={item} key={item.id} setProductQuantity={setProductQuantity} />;
       })}
       {!cart?.items.length && <p>Your cart is empty</p>}
       <div className="flex flex-col gap-3 sm:items-center md:items-end">
-        <p className="mb-3 font-bold">
-          Total: {formatPrice(cart?.subTotal || 0)}
-        </p>
+        <p className="mb-3 font-bold">Total: {formatPrice(cart?.subTotal || 0)}</p>
       </div>
 
       {cart?.items.length !== 0 && (
         <div className="flex flex-col gap-3 sm:items-center md:items-end">
-          <Link
-            href="/"
-            className="btn btn-primary  shadow-md hover:shadow-2xl sm:w-[200px]"
-          >
+          <Link href="/" className="btn btn-primary  shadow-md hover:shadow-2xl sm:w-[200px]">
             Continue shopping
           </Link>
-          <Link
-            href="/placing-an-order"
-            className="btn btn-primary  shadow-md hover:shadow-2xl sm:w-[200px]"
-          >
+          <Link href="/placing-an-order" className="btn btn-primary  shadow-md hover:shadow-2xl sm:w-[200px]">
             Placing an order
           </Link>
         </div>
