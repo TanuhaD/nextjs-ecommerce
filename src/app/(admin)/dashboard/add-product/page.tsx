a9c0ce2a15c65d9abdc994b2b68bb53a8bd4e82a
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2024-01-26 21:43:46 +0200

    page.tsx

--- src/app/(admin)/dashboard/add-product/page.tsx
+++ src/app/(admin)/dashboard/add-product/page.tsx
@@ -4,21 +4,17 @@ import UploadFromFileForm from "./UploadFromFileForm";
 import { addProduct } from "./actions";
 export const metadata = {
   metadataBase: new URL(env.BASE_URL),
-  title: "Add Product Flowmazon",
+  title: "Add Product PrimePicks",
 };
 
 export default async function AddProductPage() {
   return (
     <div>
       <h1 className="mb-4 text-lg font-bold">Add Product</h1>
-      <h2 className="mb-3 text-center text-lg font-bold">
-        Add product manually
-      </h2>
+      <h2 className="mb-3 text-center text-lg font-bold">Add product manually</h2>
       <AddUpdateProductForm action={addProduct} />
       <div className="divider"></div>
-      <h2 className="mb-3 text-center text-lg font-bold">
-        Add product from file
-      </h2>
+      <h2 className="mb-3 text-center text-lg font-bold">Add product from file</h2>
       <UploadFromFileForm />
     </div>
   );
