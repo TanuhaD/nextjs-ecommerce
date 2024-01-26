81343018a90c4ae67ea9a746af9984749bcdaf50
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2024-01-26 23:30:42 +0200

    ShoppingCartbutton.tsx

--- src/components/NavBar/ShoppingCartButton.tsx
+++ src/components/NavBar/ShoppingCartButton.tsx
@@ -11,8 +11,8 @@ interface ShoppingCartButtonProps {
 
 export default function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
   return (
-    <div className="dropdown-end dropdown-hover dropdown flex">
-      <label tabIndex={0} className="btn-ghost btn-circle btn">
+    <div className="dropdown dropdown-end dropdown-hover flex">
+      <label tabIndex={0} className="btn btn-circle btn-ghost">
         <div className="indicator">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -28,26 +28,15 @@ export default function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
               d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
             />
           </svg>
-          <span className="badge badge-sm indicator-item">
-            {cart?.size || 0}
-          </span>
+          <span className="badge indicator-item badge-sm">{cart?.size || 0}</span>
         </div>
       </label>
-      <div
-        tabIndex={0}
-        className="card dropdown-content card-compact z-30 mt-4 w-52 bg-base-100 shadow"
-      >
+      <div tabIndex={0} className="card dropdown-content card-compact z-30 mt-4 w-52 bg-base-100 shadow">
         <div className="card-body">
-          <span className="text-lg font-bold">{cart?.size || 0}Items</span>
-          <span className="text-info">
-            Subtotal{formatPrice(cart?.subTotal || 0)}
-          </span>
+          <span className="text-lg font-bold">{cart?.size || 0} Goods</span>
+          <span className="font-bold text-info">Subtotal{formatPrice(cart?.subTotal || 0)}</span>
           <div className="card-actions-ac">
-            <Link
-              href="/cart"
-              className="btn-primary btn-block btn"
-              onClick={closeDropdowns}
-            >
+            <Link href="/cart" className="btn btn-primary btn-block" onClick={closeDropdowns}>
               View Cart
             </Link>
           </div>
