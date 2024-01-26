ab738a7ef0e5c4fad63cff0ab708ee285a5856c8
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2024-01-26 23:28:15 +0200

    layout.tsx

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -9,23 +9,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   metadataBase: new URL(env.BASE_URL),
-  title: "Flowmazon",
+  title: "PrimePicks",
   description: "We make your wallet cry",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <SessionProvider>
           <Navbar />
-          <main className=" m-auto min-w-[300px] max-w-7xl p-4">
-            {children}
-          </main>
+          <main className=" m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
           <Footer />
         </SessionProvider>
       </body>
